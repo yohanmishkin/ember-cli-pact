974b123b16f47f27a9ecdb50061fa9cecb495a5c
Yohan Mishkin <yohanmishkin@protonmail.com>
2018-09-26 17:44:42 -0400

    Ember upgrade

--- tests/acceptance/departments-test.js
+++ tests/acceptance/departments-test.js
@@ -1,11 +1,13 @@
 import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { setupPact } from 'ember-cli-pact';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
 module('qwer Acceptance | departments', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
+  setupPact(hooks);
 
   test('visiting /departments', async function(assert) {
     server.createList('department', 3);
